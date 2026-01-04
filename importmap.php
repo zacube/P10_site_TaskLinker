7dbffb76579c47a2256893d24091dc82384362ad
zacube <contact@zacube.com>
2026-01-04 17:54:03 +0100

    Authentification (Authcontroller) Mise à jour des pages avec : - icone du User - bouton "déconnexion"

--- importmap.php
+++ importmap.php
@@ -16,6 +16,10 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'select' => [
+        'path' => './assets/js/select.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class AuthController extends AbstractController
 {
@@ -24,10 +25,17 @@ final class AuthController extends AbstractController
     }
 
     #[Route('/login', name: 'app_auth_login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // récupérer l'erreur de connexion si elle existe
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // récupérer le dernier email saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('auth/login.html.twig', [
-            'controller_name' => 'login',
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
 
@@ -60,6 +68,13 @@ final class AuthController extends AbstractController
         return $this->render('auth/signin.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/logout', name: 'app_auth_logout')]
+    public function logout(): void
+    {
+        // Ce code ne sera jamais exécuté
+        throw new \LogicException('This should never be reached!');
     }
 
 }
